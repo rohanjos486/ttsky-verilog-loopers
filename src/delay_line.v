4b489e385a6af29f262bd958dee704c25b4c3390
rohanjos486 <60748007+rohanjos486@users.noreply.github.com>
2026-04-11 23:45:28 +0530

    Update delay_line.v

--- src/delay_line.v
+++ src/delay_line.v
@@ -11,8 +11,7 @@ module delay_line(
     input [7:0] din,
     output reg [7:0] x1,
     output reg [7:0] x2,
-    output reg [7:0] x3,
-    output reg [7:0] x4
+    output reg [7:0] x3
     
 );
 
@@ -21,9 +20,7 @@ module delay_line(
             x1 <= 0;
             x2 <= 0;
             x3 <= 0;
-            x4 <= 0;
         end else begin
-            x4 <= x3;
             x3 <= x2;
             x2 <= x1;
             x1 <= din;
