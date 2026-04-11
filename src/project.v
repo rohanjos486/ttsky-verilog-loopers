f4fcc5d887d2a934dff3d9e875067dc780642339
rohanjos486 <60748007+rohanjos486@users.noreply.github.com>
2026-04-11 20:52:56 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_halfadder (
+module tt_um_dsp_top (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -18,10 +18,24 @@ module tt_um_halfadder (
 
   // All output pins must be assigned. If not used, assign to 0.
  // Example: ou_out is the sum of ui_in and uio_in
+     wire [7:0] x1, x2;
   assign uio_out = 0;
   assign uio_oe  = 0;
-    assign uo_out[0] = ui_in[0]^ ui_in[1];
-    assign uo_out[1] = ui_in[0]& ui_in[1];
-    assign uo_out[7:2]=6'b0;
+    // Delay line
+    delay_line u_delay (
+        .clk(clk),
+        .rst(rst_n),
+        .din(ui_in),
+        .x1(x1),
+        .x2(x2)
+    );
+
+    // FIR core
+    fir_core u_fir (
+        .din(ui_in),
+        .x1(x1),
+        .x2(x2),
+        .dout(uo_out)
+    );
 
 endmodule
