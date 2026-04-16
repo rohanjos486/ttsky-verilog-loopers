84a306335af3c9a4916c944d85e641478d01ec0e
rohanjos486 <60748007+rohanjos486@users.noreply.github.com>
2026-04-16 20:44:04 +0530

    Update fir_core.v

--- src/fir_core.v
+++ src/fir_core.v
@@ -17,6 +17,7 @@ module fir_core (
 
     // ================= FIR =================
     reg [1:0] scale;
+    reg [1:0] mode;
     wire [10:0] sum;
     wire [10:0] scaled;
 
@@ -53,14 +54,30 @@ module fir_core (
     wire signed [11:0] feedback2 = $signed({4'b0000, y_prev2}) >> 3;
 
     // ================= Final Sum =================
+    wire signed [11:0] selected_output;
+    wire signed [11:0] final_sum = selected_output;
 
-    wire signed [11:0] final_sum = combined_fir + feedback1 + feedback2;
 
+wire signed [11:0] fir2_only = fir2_ext;
+wire signed [11:0] fir_only  = fir1_ext;
+wire signed [11:0] fir_iir   = fir1_ext + feedback1 + feedback2;
+wire signed [11:0] full_out  = combined_fir + feedback1 + feedback2;
+
+
+
+wire signed [11:0] selected_output;
+
+assign selected_output =
+    (mode == 2'b00) ? fir_only  :
+    (mode == 2'b01) ? fir_iir   :
+    (mode == 2'b10) ? fir2_only :
+                      full_out;
     // ================= Sequential =================
 reg loaded;
     always @(posedge clk or posedge rst) begin
         if (rst) begin
     scale  <= 2'd2;
+    mode   <= 0;
     loaded <= 0;
     dout    <= 0;
     y_prev  <= 0;
@@ -68,24 +85,27 @@ reg loaded;
 end 
         else begin
                 if (!loaded) begin
-                  scale  <= din[1:0];
-                  loaded <= 1;
-                end
-            // Saturation
-            if (final_sum > 12'sd255)
-                dout <= 8'd255;
-            else if (final_sum < 12'sd0)
-                dout <= 8'd0;
-            else
-                dout <= final_sum[7:0];
+    mode   <= din[1:0];
+    scale  <= din[3:2];
+    loaded <= 1;
+    dout   <= 0;
+end else begin
+    // Saturation
+    if (final_sum > 12'sd255)
+        dout <= 8'd255;
+    else if (final_sum < 12'sd0)
+        dout <= 8'd0;
+    else
+        dout <= final_sum[7:0];
 
-            // Feedback shift register
-            y_prev2 <= y_prev;
-
-            y_prev <= (final_sum > 12'sd255) ? 8'd255 :
-                      (final_sum < 12'sd0)   ? 8'd0   :
-                      final_sum[7:0];
+    // Feedback
+    y_prev2 <= y_prev;
+    y_prev  <= (final_sum > 12'sd255) ? 8'd255 :
+               (final_sum < 12'sd0)   ? 8'd0   :
+               final_sum[7:0];
+end
         end
     end
 
 endmodule
+
